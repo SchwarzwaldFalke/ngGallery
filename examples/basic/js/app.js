2b3072c84b2872f6d030cdadd200a779f390248f
Christoph Bessei <github@bessei-it.eu>
2016-02-09 11:53:00 +0100

    Fix image path

--- examples/basic/js/app.js
+++ examples/basic/js/app.js
@@ -4,25 +4,25 @@ var myApp = angular.module('myApp', ['ngTouch','jkuri.gallery']);
 myApp.controller('testController', ['$scope',function($scope) {
     $scope.images = [
         {
-            thumb: 'https://raw.githubusercontent.com/SchwarzwaldFalke/ngGallery/master/images/thumbs/1.jpg',
-            img: 'https://raw.githubusercontent.com/SchwarzwaldFalke/ngGallery/master/images/1.jpg',
-            downloadSrc: 'https://raw.githubusercontent.com/SchwarzwaldFalke/ngGallery/master/images/1.jpg',
+            thumb: 'images/thumbs/1.jpg',
+            img: 'images/1.jpg',
+            downloadSrc: 'images/1.jpg',
             description: 'Image 1'
         },
         {
-            thumb: 'https://raw.githubusercontent.com/SchwarzwaldFalke/ngGallery/master/images/thumbs/2.jpg',
-            img: 'https://raw.githubusercontent.com/SchwarzwaldFalke/ngGallery/master/images/2.jpg',
-            downloadSrc: 'https://raw.githubusercontent.com/SchwarzwaldFalke/ngGallery/master/images/2.jpg',
+            thumb: 'images/thumbs/2.jpg',
+            img: 'images/2.jpg',
+            downloadSrc: 'images/2.jpg',
             description: 'Image 2'
 
         },
         {
-            thumb: 'https://raw.githubusercontent.com/SchwarzwaldFalke/ngGallery/master/images/thumbs/3.jpg',
-            img: 'https://raw.githubusercontent.com/SchwarzwaldFalke/ngGallery/master/images/3.jpg',
-            downloadSrc: 'https://raw.githubusercontent.com/SchwarzwaldFalke/ngGallery/master/images/3.jpg',
+            thumb: 'images/thumbs/3.jpg',
+            img: 'images/3.jpg',
+            downloadSrc: 'images/3.jpg',
             description: 'Image 3'},
         {
-            thumb: 'https://raw.githubusercontent.com/SchwarzwaldFalke/ngGallery/master/images/thumbs/4.jpg',
-            img: 'https://raw.githubusercontent.com/SchwarzwaldFalke/ngGallery/master/images/4.jpg',  description: 'Image 4'}
+            thumb: 'images/thumbs/4.jpg',
+            img: 'images/4.jpg',  description: 'Image 4'}
     ];
 }]);
